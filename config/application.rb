d14866a054e1110c3015e10970939112d1cd18a3
Athika Jishida <athikajishida@gmail.com>
2024-03-21 15:42:49 +0530

    rendersetup

--- config/application.rb
+++ config/application.rb
@@ -8,35 +8,33 @@ Bundler.require(*Rails.groups)
 
 module MyFirstProject
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    config.to_prepare do
-      Devise::SessionsController.layout "user_layouts"
-      Devise::RegistrationsController.layout "user_layouts"
-      Devise::ConfirmationsController.layout "user_layouts"
-      Devise::UnlocksController.layout "user_layouts"            
-      Devise::PasswordsController.layout "user_layouts"
-    end
-
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-    config.action_mailer.delivery_method = :smtp
-
-    ActionMailer::Base.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: '587',
-      authentication: :plain,
-      user_name: 'athikajishida@gmail.com',
-      password: "yopg jilu hcis tmtp",
-      domain: 'render.com',
-      enable_starttls_auto: true
-    }
-
+    config.autoload_lib(ignore: %w(assets tasks))
+     config.to_prepare do
+    Devise::SessionsController.layout "user_layouts"
+    Devise::RegistrationsController.layout "user_layouts"
+    Devise::ConfirmationsController.layout "user_layouts"
+    Devise::UnlocksController.layout "user_layouts"            
+    Devise::PasswordsController.layout "user_layouts"
+    
+  end
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'athikajishida@gmail.com',
+    password: '8129272229',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+      
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
