12a7584c3abc45878c87aeccd0a2ac288f293542
Athika Jishida <athikajishida@gmail.com>
2024-03-21 18:12:47 +0530

    activestorage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,8 @@ Rails.application.configure do
 
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
-  config.active_storage.service = :disk
+  # config.active_storage.service = :disk
+  config.active_storage.service = :render
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
