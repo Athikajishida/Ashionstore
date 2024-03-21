65d5e65a5a078c419707d4be4fd1b4e550098981
Athika Jishida <athikajishida@gmail.com>
2024-03-21 13:10:51 +0530

    smtps

--- config/application.rb
+++ config/application.rb
@@ -26,15 +26,23 @@ module MyFirstProject
   end
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: 'athikajishida@gmail.com',
-    password: '8129272229',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
-      
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   user_name: 'athikajishida@gmail.com',
+  #   password: '8129272229',
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  # }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => athikajishida@gmail.com,
+    :password       => "yopg jilu hcis tmtp",
+    :domain         => 'render.com',
+    :enable_starttls_auto => true
+} 
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
